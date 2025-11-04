b6a36e1a1a7b5a47ad1ae244b04f6271f90be16f
Enzo Giaquinta <enzo.n.giaquinta@gmail.com>
2025-11-04 18:16:21 -0300

    cambios importantes en verificar_jerarquia

--- funcionalidades/verificar_jerarquia.py
+++ funcionalidades/verificar_jerarquia.py
@@ -1,6 +1,8 @@
 import os
+import csv
 
 RUTA_BASE = "Supermercado"
+NOMBRE_CSV = "Productos.csv"
 
 estructura = {
     "Alimentos": {"Lacteos": ("Entero", "Descremado"), 
@@ -9,23 +11,36 @@ estructura = {
                 "Gaseosas": ("Regular", "Zero")},
 }
 
+def verificar_existencia_csv(ruta: str) -> None:
+    if os.path.exists(ruta):
+        print(f"Existe {ruta}")
+    else:
+        print(f"No existe {ruta}")
+        with open(ruta, "w", newline="", encoding="utf-8") as archivo:
+            encabezados = ["ID", "Nombre", "Precio", "Stock"]
+            escritor = csv.DictWriter(archivo, fieldnames=encabezados)
+            escritor.writeheader()
+
 def verificar_existencia(ruta: str, subestructura: dict) -> None:
     """Verifica la existencia de carpetas."""
     if not os.path.exists(ruta):
         print(f"No existe carpeta: {ruta}")
-        return
-    
-    print(f"Existe carpeta: {ruta}")
+        os.mkdir(ruta)
+    else:
+        print(f"Existe carpeta: {ruta}")
 
     # Recorre los subniveles (si los hay)
     for carpeta, subcarpetas in subestructura.items():
-        for subcapeta, sub_cap in subcarpetas.items():
-            for sub in sub_cap:
-                nueva_ruta = os.path.join(ruta, carpeta, subcapeta , sub)
-                if os.path.exists(nueva_ruta):
-                    print(f"Existe {nueva_ruta}")
-                else:
-                    print(f"Falta {nueva_ruta}")
+        print(carpeta)
+        if type(subcarpetas) == tuple:
+            for sub_directorio in subcarpetas:
+                nueva_ruta = os.path.join(ruta, carpeta ,sub_directorio, NOMBRE_CSV)
+                os.makedirs(os.path.dirname(nueva_ruta), exist_ok=True)
+                verificar_existencia_csv(nueva_ruta)
+        else:
+            nueva_ruta = os.path.join(ruta, carpeta)
+            verificar_existencia(nueva_ruta, subcarpetas)
+                    
 
 def main():
     verificar_existencia(RUTA_BASE, estructura)
