bab2fc2cf6fa8450729176d7b26d295f85544b35
Enzo Giaquinta <enzo.n.giaquinta@gmail.com>
2025-11-04 18:51:47 -0300

    agregado validar_csv a verificar_jerarquia

--- funcionalidades/verificar_jerarquia.py
+++ funcionalidades/verificar_jerarquia.py
@@ -3,6 +3,7 @@ import csv
 
 RUTA_BASE = "Supermercado"
 NOMBRE_CSV = "Productos.csv"
+ENCABEZADOS = ["ID", "Nombre", "Precio", "Stock"]
 
 estructura = {
     "Alimentos": {"Lacteos": ("Entero", "Descremado"), 
@@ -11,27 +12,59 @@ estructura = {
                 "Gaseosas": ("Regular", "Zero")},
 }
 
+def crear_csv(ruta: str) -> None:
+    with open(ruta, "w", newline="", encoding="utf-8") as archivo:
+            
+            escritor = csv.DictWriter(archivo, fieldnames=ENCABEZADOS)
+            escritor.writeheader()
+
+def nombre_correcto(nombre: str) -> bool:
+    return nombre.strip().replace(" ", "")
+
+def numero_correcto(numero: str) -> bool:
+    return numero.isdigit() and int(numero) >= 0
+
+def validar_csv(ruta: str) -> bool:
+    lineas_correctas = []
+    linea_erronea = False
+    with open(ruta, "r", newline="", encoding="utf-8") as archivo:
+        lector = csv.DictReader(archivo)
+        if lector.fieldnames == ENCABEZADOS:
+            contador = 1
+            for producto in lector:
+                contador += 1
+                if not nombre_correcto(producto["Nombre"]) or not numero_correcto(producto["ID"]) or not numero_correcto(producto["Precio"]) or not numero_correcto(producto["Stock"]):
+                    print(f"Archivo: {ruta}, Linea: {contador} inválida.")
+                    linea_erronea = True
+                else:
+                    lineas_correctas.append(producto)
+        else:
+            print(f"Archivo: {ruta}, Encabezados inválidos")
+            crear_csv(ruta)
+        
+    if linea_erronea:
+        with open(ruta, "w", newline="", encoding="utf-8") as archivo:
+            escritor = csv.DictWriter(archivo, fieldnames=ENCABEZADOS)
+            escritor.writeheader()
+            escritor.writerows(lineas_correctas)
+        print(f"Archivo: {ruta}, validado y corregido")
+
 def verificar_existencia_csv(ruta: str) -> None:
     if os.path.exists(ruta):
         print(f"Existe {ruta}")
+        validar_csv(ruta)
     else:
         print(f"No existe {ruta}")
-        with open(ruta, "w", newline="", encoding="utf-8") as archivo:
-            encabezados = ["ID", "Nombre", "Precio", "Stock"]
-            escritor = csv.DictWriter(archivo, fieldnames=encabezados)
-            escritor.writeheader()
+        crear_csv(ruta)
 
 def verificar_existencia(ruta: str, subestructura: dict) -> None:
     """Verifica la existencia de carpetas."""
     if not os.path.exists(ruta):
         print(f"No existe carpeta: {ruta}")
         os.mkdir(ruta)
-    else:
-        print(f"Existe carpeta: {ruta}")
 
     # Recorre los subniveles (si los hay)
     for carpeta, subcarpetas in subestructura.items():
-        print(carpeta)
         if type(subcarpetas) == tuple:
             for sub_directorio in subcarpetas:
                 nueva_ruta = os.path.join(ruta, carpeta ,sub_directorio, NOMBRE_CSV)
